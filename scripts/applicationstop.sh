d82a344a71a2f299987b6d5639545537a38213d5
sumzz <sumzz>
2023-11-01 13:17:17 +0000

    added shell scripts

---
+++ scripts/applicationstop.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+if [ -f '/usr/lib/systemd/system/httpd.service' ]; then 
+sudo systemctl stop httpd
+else
+sudo yum install -y httpd
+fi 
---
+++ scripts/beforeinstall.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+if [ -d "/home/ec2-user/wordpress/" ]; then
+sudo rm -r -f /home/ec2-user/wordpress/*
+else
+sudo mkdir -p /home/ec2-user/wordpress/
+fi
+
+if [ -d "/var/www/wordpress" ]; then
+sudo rm -r -f /var/www/wordpress/*
+else
+sudo mkidr /var/www/wordpress/ 
+fi
+
