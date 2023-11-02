3bacbfae737c32d37fc89cd2f7ce61646cc324cf
summzzgit <sumitkotabagi@gmail.com>
2023-11-02 16:33:46 +0530

    edited shell script

--- scripts/afterinstall.sh
+++ scripts/afterinstall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo unzip /home/ec2-user/wordpress/*.zip
 sudo rm -f /home/ec2-user/wordpress/*.zip
-sudo cp /home/ec2-user/wordpress/* /var/www/wordpress/
+sudo cp -r /home/ec2-user/wordpress/* /var/www/wordpress/
 sudo cp /etc/httpd/conf/httpd.conf /etc/httpd/conf/httpd.conf.backup
 sudo sudo sed -i '124c\ DocumentRoot "/var/www/wordpress"' /etc/httpd/conf/httpd.conf
 sudo sudo sed -i '169c\ DirectoryIndex index.php index.html' /etc/httpd/conf/httpd.conf
