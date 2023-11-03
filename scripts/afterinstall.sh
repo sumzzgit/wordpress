d8cd1f66d89c6d2457a537e6b5169f315c68ed8d
summzzgit <sumitkotabagi@gmail.com>
2023-11-03 15:18:03 +0530

    .

--- scripts/afterinstall.sh
+++ scripts/afterinstall.sh
@@ -2,11 +2,12 @@
 #sudo unzip /home/ec2-user/wordpress/*.zip
 #sudo rm -f /home/ec2-user/wordpress/*.zip
 # sudo cp -r /home/ec2-user/wordpress/*.yml /usr/share/nginx/wordpress/
-sudo cp -r /home/ec2-user/wordpress/*.yml /home/ec2-user/test/
-sudo cp -r /home/ec2-user/wordpress/scripts /home/ec2-user/test/
-sudo cp -r /home/ec2-user/wordpress/nginx.co* /etc/nginx/
+sudo mv -r /home/ec2-user/wordpress/*.yml /home/ec2-user/test/
+sudo mv -r /home/ec2-user/wordpress/scripts /home/ec2-user/test/
+sudo mv -r /home/ec2-user/wordpress/nginx.co* /etc/nginx/
 sudo cp -r /home/ec2-user/wordpress/* /usr/share/nginx/wordpress/
+sudo chown -R nginx:nginx /usr/share/nginx/wordpress
 #sudo cp /etc/httpd/conf/httpd.conf /etc/httpd/conf/httpd.conf.backup
 #sudo sudo sed -i '124c\ DocumentRoot "/var/www/wordpress"' /etc/httpd/conf/httpd.conf
 #sudo sudo sed -i '169c\ DirectoryIndex index.php index.html' /etc/httpd/conf/httpd.conf
-sudo systemctl start nginx
+sudo systemctl start nginx 
