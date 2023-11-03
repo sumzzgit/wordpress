6dd9283414fcfea924a79c3a2c0ae9a0414a01d4
summzzgit <sumitkotabagi@gmail.com>
2023-11-03 13:01:03 +0530

    made some changes to scripts

--- scripts/applicationstop.sh
+++ scripts/applicationstop.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
-if [[ -f '/usr/lib/systemd/system/httpd.service' ]]; then 
-sudo systemctl stop httpd
+#stop nginx if present or else install nginx
+if [[ -f '/usr/lib/systemd/system/nginx.service' ]]; then
+    sudo systemctl stop nginx
 else
-sudo yum install -y httpd
-fi 
+    sudo yum install -y nginx
+fi
+#stop mysql service if present or else install mysql but here we are using RDS
+if [[ -f '/usr/lib/systemd/system/mysqld.service' ]]; then
+    sudo systemctl restart mysqld
+else
+    cd /home/ec2-user/
+    sudo wget https://dev.mysql.com/get/mysql80-community-release-el9-1.noarch.rpm
+    sudo dnf install mysql80-community-release-el9-1.noarch.rpm -y
+    sudo dnf install mysql-community-server -y
+    sudo systemctl start mysqld
+fi
+
--- scripts/beforeinstall.sh
+++ scripts/beforeinstall.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 if [[ -d "/home/ec2-user/wordpress/" ]]; then
-sudo rm -r -f /home/ec2-user/wordpress/*
+    sudo rm -r -f /home/ec2-user/wordpress/*
 else
-sudo mkdir -p /home/ec2-user/wordpress/
+    sudo mkdir -p /home/ec2-user/wordpress
 fi
 
-if [[ -d "/var/www/wordpress" ]]; then
-sudo rm -r -f /var/www/wordpress/*
+if [[ -d "/home/ec2-user/wordpress/test" ]]; then
+    sudo rm -r -f /home/ec2-user/wordpress/test/*
 else
-sudo mkdir /var/www/wordpress
+    sudo mkdir -p /home/ec2-user/wordpress/test
 fi
 
+if [[ -d "/usr/share/nginx/wordpress" ]]; then
+    sudo rm -r -f /usr/share/nginx/wordpress/*
+else
+    sudo mkdir -p /usr/share/nginx/wordpress
+    sudo chown -R nginx:nginx /usr/share/nginx/wordpress
+fi
+
+#instaling php dependencies
+sudo yum install php8.1.x86_64 php8.1-gd.x86_64 php8.1-soap.x86_64 php8.1-intl.x86_64 php8.1-mysqlnd.x86_64 php8.1-pdo.x86_64 php8.1-fpm.x86_64 php8.1-odbc.x86_64 php8.1-common.x86_64 php8.1-xml.x86_64 -y
