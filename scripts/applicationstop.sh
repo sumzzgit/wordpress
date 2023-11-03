51e838721c563140099cc8d396930b1805eb7ea4
summzzgit <sumitkotabagi@gmail.com>
2023-11-03 15:05:12 +0530

    .

--- scripts/applicationstop.sh
+++ scripts/applicationstop.sh
@@ -4,6 +4,7 @@ if [[ -f '/usr/lib/systemd/system/nginx.service' ]]; then
     sudo systemctl stop nginx
 else
     sudo yum install -y nginx
+    sleep 30
 fi
 #stop mysql service if present or else install mysql but here we are using RDS
 if [[ -f '/usr/lib/systemd/system/mysqld.service' ]]; then
@@ -13,6 +14,7 @@ else
     sudo wget https://dev.mysql.com/get/mysql80-community-release-el9-1.noarch.rpm
     sudo dnf install mysql80-community-release-el9-1.noarch.rpm -y
     sudo dnf install mysql-community-server -y
+    sleep 30
     sudo systemctl start mysqld
 fi
 
