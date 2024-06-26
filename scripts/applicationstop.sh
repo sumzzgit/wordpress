ff27df54f41b1a975e44ec8244dc88e30f2095dc
sumzzgit <131842007+sumzzgit@users.noreply.github.com>
2024-06-26 16:51:36 +0530

    Update applicationstop.sh

--- scripts/applicationstop.sh
+++ scripts/applicationstop.sh
@@ -11,8 +11,10 @@ if [[ -f '/usr/lib/systemd/system/mysqld.service' ]]; then
     sudo systemctl restart mysqld
 else
     cd /home/ec2-user/
-    sudo wget https://dev.mysql.com/get/mysql80-community-release-el9-1.noarch.rpm
+    sudo wget https://dev.mysql.com/get/mysql80-community-release-el9-1.noarch.rpm 
     sudo dnf install mysql80-community-release-el9-1.noarch.rpm -y
+    sudo rpm --import https://repo.mysql.com/RPM-GPG-KEY-mysql-2023
+    sudo dnf install mysql-community-client -y
     sudo dnf install mysql-community-server -y
     sleep 30
     sudo systemctl start mysqld
